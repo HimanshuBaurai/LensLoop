5a79c2eb0aacdc959fb8e6f39ecbc8ac2bdce4d8
Himanshu Baurai <himanshucosmicera@gmail.com>
2024-02-01 01:08:07 +0530

    added various components for home page,and made it ready

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,13 +1,25 @@
 //similar to AuthLayout page we will have a RootLayout page
 //this page will have a navbar,sidebar,etc, and the pages will change inside it depending on the route
 
+import { Outlet } from "react-router-dom";
 
-import React from 'react'
+import Topbar from "@/components/shared/Topbar";
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
 
 const RootLayout = () => {
     return (
-        <div>RootLayout</div>
-    )
-}
+        <div className="w-full md:flex">
+            <Topbar />
+            <LeftSidebar />
 
-export default RootLayout
+            <section className="flex flex-1 h-full">
+                <Outlet />
+            </section>
+
+            <Bottombar />
+        </div>
+    );
+};
+
+export default RootLayout;
