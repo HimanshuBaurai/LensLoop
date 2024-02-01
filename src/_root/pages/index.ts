0f7a41c9ea2dbe7fe4058cdb6f5deeb5f6602f37
Himanshu Baurai <himanshucosmicera@gmail.com>
2024-02-01 22:29:46 +0530

    some fine refinements

--- src/_root/pages/index.ts
+++ src/_root/pages/index.ts
@@ -1,4 +1,13 @@
 //as we have many pages so we dont want to import them one by one
 //so we will use index.ts file to import all the pages and export them
 
-export { default as Home } from './Home';
+export { default as Home } from "./Home";
+export { default as Explore } from "./Explore";
+export { default as Saved } from "./Saved";
+export { default as CreatePost } from "./CreatePost";
+export { default as Profile } from "./Profile";
+export { default as UpdateProfile } from "./UpdateProfile";
+export { default as EditPost } from "./EditPost";
+export { default as PostDetails } from "./PostDetails";
+export { default as LikedPosts } from "./LikedPosts";
+export { default as AllUsers } from "./AllUsers";
